34397a9480056968988df0883ffe69b0ec5f439c
LimHyun83 <67883778+LimHyun83@users.noreply.github.com>
2020-08-19 17:03:11 +0900

    commit
    commit

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = '여기에 스크래핑할 영화 주소가 들어간다.'
-
+# URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-data = requests.get(url,headers=headers)
+data = requests.get('http://www.cine21.com/db/writer/info/?pre_code=E20041947', headers=headers)
 
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
 soup = BeautifulSoup(data.text, 'html.parser')
 
-from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.dbreview
+# select를 이용해서, tr들을 불러오기
+reviews = soup.select('#write_view_review20_holder > table > tbody > tr')
+
+# reviews (tr들) 의 반복문을 돌리기
+for review in reviews:
+    # review안에 a가 있으며느,
+    a_tag = review.select_one('td:nth-child(3) > p.mov_tit > a')
+    if a_tag is not None:
+        movie_title = a_tag.text
+        print(movie_title)
+
